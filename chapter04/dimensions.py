83e16ac56227b5efde56add0ee058b0ff34b5857
Socheat <horlsocheat@gmail.com>
2021-12-06 16:33:53 +0700

    End of Chapter 4

---
+++ chapter04/dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+#dimensions[0] = 250
+# print(dimensions[0])
+# print(dimensions[1])
+print("Original dimensions:")
+for dimension in dimensions:
+  print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+  print(dimension)
